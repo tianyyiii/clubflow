3d4d0c2f2073f524925e7ddc0c7d77e909331ccd
control-cyber <74284821+control-cyber@users.noreply.github.com>
2022-12-05 19:37:17 +0800

    the static controller part

---
+++ version0.0.1/club_project/club/src/main/java/com/example/club/controller/ClubController.java
@@ -0,0 +1,85 @@
+package com.example.club.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.club.utils.ImageUtil;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags="club api")
+@RestController
+@RequestMapping("/club")
+public class ClubController {
+    @Autowired
+    private ImageUtil imageUtil;
+    @PutMapping("/create")
+    public JSONObject createClub(JSONObject inform,int UserId){
+        JSONObject state=new JSONObject();
+        state.put("state",1);
+        state.put("club id",800);
+        return state;
+
+    }
+    @PostMapping("/modify")
+    public JSONObject modifyClub(JSONObject inform,int ClubId,int UserId){
+        JSONObject state=new JSONObject();
+        state.put("state",1);
+        return state;
+    }
+    @GetMapping("/view")
+    public JSONObject viewClub(int ClubId,int UserId){
+        JSONObject club=new JSONObject();
+        club.put("name","800号电影社");
+        club.put("category","文化艺术类");
+        club.put("fans number",10000);
+        club.put("publications number",100);
+        club.put("comments number",50);
+        club.put("inform","这是一个电影社团");
+        club.put("profile",imageUtil.ImageToBase64("C:\\Users\\26010\\Downloads\\club_project\\image\\tarkovsky.png"));
+        club.put("created time","2010-10-2");
+        club.put("subscribe",1);
+        return club;
+    }
+    @GetMapping("/view_by_account")
+    public JSONObject viewClubbyAccount(int UserId){
+        JSONObject ClubList=new JSONObject();
+        for (int i=1;i<3;i++)
+        {   JSONObject club=new JSONObject();
+            club.put("name","800号电影社");
+            club.put("category","文化艺术类");
+            club.put("fans number",10000);
+            club.put("publications number",100);
+            club.put("comments number",50);
+            club.put("inform","这是一个电影社团");
+            club.put("profile",imageUtil.ImageToBase64("C:\\Users\\26010\\Downloads\\club_project\\image\\tarkovsky.png"));
+            club.put("created time","2010-10-2");
+            club.put("subscribe",1);
+            ClubList.put("club"+Integer.toString(i),club);
+        }
+        return ClubList;
+
+
+    }
+    @GetMapping("/view_announcement")
+    public JSONObject viewAnnouncement(int UserId){
+        JSONObject AnnouncementList=new JSONObject();
+        for (int i=1;i<3;i++)
+        {   JSONObject Announcement=new JSONObject();
+            Announcement.put("context","塔可夫斯基《乡愁》放映");
+            AnnouncementList.put("announcement"+Integer.toString(i),Announcement);
+        }
+        return AnnouncementList;
+
+
+    }
+
+/*
+    @Autowired
+    private ClubService clubservice;
+*/
+
+
+
+
+
+}
