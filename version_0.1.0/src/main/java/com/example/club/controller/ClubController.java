c9adfd2a8eaf0246ed6095a70dedf446d8040966
control-cyber <hb4233@sjtu.edu.cn>
2022-12-11 21:26:34 +0800

    controller change

--- version_0.1.0/src/main/java/com/example/club/controller/ClubController.java
+++ version_0.1.0/src/main/java/com/example/club/controller/ClubController.java
@@ -1,6 +1,7 @@
 package com.example.club.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.club.service.Impl.ClubService;
 import com.example.club.utils.ImageUtil;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +13,27 @@ import org.springframework.web.bind.annotation.*;
 public class ClubController {
     @Autowired
     private ImageUtil imageUtil;
+    @Autowired
+    private ClubService clubService;
     @PutMapping("/create")
     public JSONObject createClub(JSONObject inform,int UserId){
-        JSONObject state=new JSONObject();
+/*        JSONObject state=new JSONObject();
         state.put("state",1);
         state.put("club id",800);
-        return state;
+        return state;*/
+        return clubService.createClub(inform,UserId);
 
     }
     @PostMapping("/modify")
     public JSONObject modifyClub(JSONObject inform,int ClubId,int UserId){
-        JSONObject state=new JSONObject();
+/*        JSONObject state=new JSONObject();
         state.put("state",1);
-        return state;
+        return state;*/
+        return clubService.modifyClub(inform,ClubId,UserId);
     }
     @GetMapping("/view")
     public JSONObject viewClub(int ClubId,int UserId){
-        JSONObject club=new JSONObject();
+/*        JSONObject club=new JSONObject();
         club.put("name","800号电影社");
         club.put("category","文化艺术类");
         club.put("fans number",10000);
@@ -38,11 +43,12 @@ public class ClubController {
         club.put("profile",imageUtil.ImageToBase64("C:\\Users\\26010\\Downloads\\club_project\\image\\tarkovsky.png"));
         club.put("created time","2010-10-2");
         club.put("subscribe",1);
-        return club;
+        return club;*/
+        return clubService.viewClub(ClubId,UserId);
     }
     @GetMapping("/view_by_account")
     public JSONObject viewClubbyAccount(int UserId){
-        JSONObject ClubList=new JSONObject();
+/*        JSONObject ClubList=new JSONObject();
         for (int i=1;i<3;i++)
         {   JSONObject club=new JSONObject();
             club.put("name","800号电影社");
@@ -56,13 +62,14 @@ public class ClubController {
             club.put("subscribe",1);
             ClubList.put("club"+Integer.toString(i),club);
         }
-        return ClubList;
+        return ClubList;*/
+        return clubService.viewClubbyAccount(UserId);
 
 
     }
     @GetMapping("/view_announcement")
-    public JSONObject viewAnnouncement(int UserId){
-        JSONObject AnnouncementList=new JSONObject();
+    public JSONObject viewAnnouncement(int UserId) {
+/*        JSONObject AnnouncementList=new JSONObject();
         for (int i=1;i<3;i++)
         {   JSONObject Announcement=new JSONObject();
             Announcement.put("context","塔可夫斯基《乡愁》放映");
@@ -71,6 +78,8 @@ public class ClubController {
         return AnnouncementList;
 
 
+    }*/
+        return clubService.viewAnnouncement(UserId);
     }
 
 /*
--- version_0.1.0/src/main/java/com/example/club/controller/PostController.java
+++ version_0.1.0/src/main/java/com/example/club/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.club.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.example.club.service.Impl.PostService;
 import com.example.club.utils.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +11,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/post")
 public class PostController {
     @Autowired
+    private PostService postService;
+    @Autowired
     private ImageUtil imageUtil;
     @PutMapping("/create")
     public JSONObject createPost(JSONObject inform,int UserId){
-        JSONObject state=new JSONObject();
+
+/*        JSONObject state=new JSONObject();
         inform.put("state",1);
         inform.put("PostId",10);
-        return inform;
+        return inform;*/
+        return postService.createPost(inform,UserId);
+
     }
     @PostMapping("/modify")
     public JSONObject modifyPost(JSONObject inform,int PostId,int UserId){
-        JSONObject state=new JSONObject();
+/*        JSONObject state=new JSONObject();
         inform.put("state",1);
-        return inform;
+        return inform;*/
+        return postService.modifyPost(inform, PostId, UserId);
     }
     @GetMapping("/{PostId}")
     public JSONObject viewPost(@PathVariable int PostId,int UserId){
-        JSONObject post=new JSONObject();
+/*        JSONObject post=new JSONObject();
         post.put("title","流动社团");
         post.put("context","这是关于流动社团的介绍");
         post.put("thumbs-up num",10);
@@ -36,11 +43,12 @@ public class PostController {
         post.put("club profile",imageUtil.ImageToBase64("C:\\Users\\26010\\Downloads\\club_project\\image\\tarkovsky.png"));
         post.put("image", imageUtil.ImageToBase64("C:\\Users\\26010\\Downloads\\club_project\\image\\tarkovsky.png"));
         post.put("club id",800);
-        return post;
+        return post;*/
+        return postService.viewPost(PostId, UserId);
     }
     @GetMapping("/view_by_user")
     public JSONObject viewPostbyUser(int UserId){
-        JSONObject post_list=new JSONObject();
+/*        JSONObject post_list=new JSONObject();
         for (int i=1;i<3;i++)
         {JSONObject post=new JSONObject();
             post.put("title","流动社团");
@@ -56,11 +64,12 @@ public class PostController {
 
 
 
-        }
-        return post_list;
+        }*/
+/*        return post_list;*/
+        return postService.viewPostbyUser(UserId);
     }
     @GetMapping("view_by_club")
-    public JSONObject viewPostbyCLub(int ClubId,int UserId){
+    public JSONObject viewPostbyCLub(int ClubId,int UserId){/*
         JSONObject post_list=new JSONObject();
         for (int i=1;i<3;i++)
         {JSONObject post=new JSONObject();
@@ -78,7 +87,8 @@ public class PostController {
 
 
         }
-        return post_list;
+        return post_list;*/
+        return postService.viewPostbyCLub(ClubId, UserId);
 
     }
 }
