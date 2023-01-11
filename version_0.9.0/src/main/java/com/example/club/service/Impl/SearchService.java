a6c768748a0df0b5ff3abf896b68bd7d3e5c6b52
shenaiyao <110119049+shenaiyao@users.noreply.github.com>
2023-01-11 21:03:34 +0800

    Add files via upload

--- version_0.9.0/src/main/java/com/example/club/service/Impl/SearchService.java
+++ version_0.9.0/src/main/java/com/example/club/service/Impl/SearchService.java
@@ -19,7 +19,7 @@ public class SearchService {
 
     public JSONObject searchforClub(JSONObject inform){
         String key=inform.getString("key");
-        String sql="SELECT * from club where clubName like ?";
+        String sql="SELECT * from club where match(clubName,clubInfo) against(?)";
         key="%"+key+"%";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,key);
         JSONObject res = new JSONObject();
@@ -30,10 +30,23 @@ public class SearchService {
         return res;
     }
 
+    public JSONObject searchforHabbit(JSONObject inform){
+        String key=inform.getString("key");
+        String sql="SELECT * from habbit where match(habbitName,habbitInfo) against(?)";
+        key="%"+key+"%";
+        List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,key);
+        JSONObject res = new JSONObject();
+        for (int i=0; i<list.size(); i++){
+            JSONObject club = new JSONObject(list.get(i));
+            res.put("habbit"+Integer.toString(i),club);
+        }
+        return res;
+    }
+
     public JSONObject searchforPost(JSONObject inform){
         String key=inform.getString("key");
         key="%"+key+"%";
-        String sql="SELECT * from post where title like '%?%' or context like ?";
+        String sql="SELECT * from post where match(title,context) against(?)";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,key);
         JSONObject res = new JSONObject();
         for (int i=0; i<list.size(); i++){
